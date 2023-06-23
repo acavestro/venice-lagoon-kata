8a86562d36a9cee1a58a9868dc4271cc5cc6cf26
Matteo Joliveau <matteo@matteojoliveau.com>
2023-06-23 15:48:50 +0200

    Add notification templating
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod test {
+    use rstest::rstest;
+
     use mockall::{automock, predicate::*};
     #[derive(Clone)]
     struct Measurement {
@@ -14,6 +16,18 @@ mod test {
                 date: date.to_string(),
                 time: time.to_string(),
                 level,
+            }
+        }
+// * **Yellow warning**: the water level is above of the lowest place in Venice, which is 80cm high
+// * **Orange warning**: the water level is above Rialto area height, which is 105cm high. It means that 5% of the city is flooded.
+// * **Red warning**: the water level is above Train Station height (Santa Lucia), which is 135cm high. Almost 50% of the city is flooded
+
+        fn level_as_str(&self) -> &str {
+            match self.level {
+                level if level < 80 => "green",
+                level if level >= 80 && level < 105 => "yellow",
+                level if level >= 105 && level < 135 => "orange",
+                _ => "red",
             }
         }
     }
@@ -41,13 +55,19 @@ mod test {
     }
 
     impl Notification {
-        fn new(text: impl ToString) -> Self {
+        fn from_text(text: impl ToString) -> Self {
             Self {
                 text: text.to_string(),
             }
         }
-        fn from_measurement(measurement: Measurement) -> Option<Self> {
-            Some(Self::new(""))
+        fn new(measurement: &Measurement, subscriber: &Subscriber) -> Option<Self> {
+            Some(Self::from_text(format!(
+                r#"Hello {},
+            today the high tide is forecast to be at {} warning level. The highest peak will be at {}."#,
+                subscriber.name, 
+                measurement.level_as_str(),
+                measurement.time,
+            )))
         }
     }
 
@@ -73,10 +93,10 @@ mod test {
         fn notify(&self) -> Result<(), String> {
             let measurements = self.measurements.get()?;
             let subscribers = self.subscribers.get()?;
-            self.sender.send(
-                subscribers.first().unwrap().clone(),
-                Notification::from_measurement(measurements.first().unwrap().clone()).unwrap(),
-            )
+            let subscriber = subscribers.first().unwrap().clone();
+            let notification =
+                Notification::new(measurements.first().unwrap(), &subscriber).unwrap();
+            self.sender.send(subscriber, notification)
         }
     }
 
@@ -95,11 +115,20 @@ mod test {
         fn get(&self) -> Result<Vec<Measurement>, String>;
     }
 
-    #[test]
-    fn subscribers_receive_notification_given_a_measurement_for_today() {
-        let measurement = Measurement::new("2023-06-01", "04:15", -15);
-        let subscriber = Subscriber::new("Foo Bar", "foo@bar.com", "3331234567");
-
+    #[rstest]
+    #[case(Measurement::new("2023-06-1", "04:15", 80), Subscriber::new("Foo Bar", "foo@bar.com", "3331234567"), Notification::from_text(
+            r#"Hello Foo Bar,
+            today the high tide is forecast to be at yellow warning level. The highest peak will be at 04:15."#,
+        ))]
+    #[case(Measurement::new("2023-06-1", "10:10", 110), Subscriber::new("Tizio Caso", "foo@bar.com", "3331234567"), Notification::from_text(
+            r#"Hello Tizio Caso,
+            today the high tide is forecast to be at orange warning level. The highest peak will be at 10:10."#,
+        ))]
+    fn subscribers_receive_notification_given_a_measurement_for_today(
+        #[case] measurement: Measurement,
+        #[case] subscriber: Subscriber,
+        #[case] expected_notification: Notification,
+    ) {
         let expected_subscriber = subscriber.clone();
 
         let mut measurements = MockMeasurements::new();
@@ -113,11 +142,6 @@ mod test {
             .expect_get()
             .times(1)
             .return_once(move || Ok(vec![subscriber]));
-
-        let expected_notification = Notification::new(
-            r#"Hello Foo Bar,
-            today the high tide is forecast to be at yellow warning level. The highest peak will be at {time}."#,
-        );
 
         let mut sender = MockSender::new();
         sender
@@ -133,6 +157,17 @@ mod test {
         );
 
         let result = notifier.notify();
+        assert!(result.is_ok());
+    }
+
+
+    #[rstest]
+    #[case::green(Measurement::new("2023-06-1", "04:15", 30), "green")]
+    #[case::yellow(Measurement::new("2023-06-1", "04:15", 85), "yellow")]
+    #[case::orange(Measurement::new("2023-06-1", "04:15", 120), "orange")]
+    #[case::red(Measurement::new("2023-06-1", "04:15", 200), "red")]
+    fn it_renders_the_level_as_a_warning_string(#[case] measurement: Measurement, #[case] expected_warning: &str) {
+        assert_eq!(expected_warning, measurement.level_as_str());
     }
 
     //#[test]
